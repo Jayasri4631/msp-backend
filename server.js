ff457f4be94a564f3f9e9c30b8634cff5d77420a
Jayasri4631 <jayasriravanan1@gmail.com>
2025-03-14 16:27:56 +0530

    Update server.js

--- server.js
+++ server.js
@@ -41,5 +41,5 @@ app.get("/", async (req, res) => {
 
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
-  console.log(Server running on port ${PORT});
+  console.log(`Server running on port ${PORT}`);
 });
